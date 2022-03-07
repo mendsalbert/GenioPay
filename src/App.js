5aa4362752b4c9395972d81d0fc5aac2540a8175
graphica99 <mendsalbert@gmail.com>
2022-03-07 16:52:15 +0000

    genopay ui

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ const App = () => {
   return (
     <Layout>
       <nav className=" w-full bg-white-400 ">
-        <div className="px-4 py-2 bg-white flex flex-row  items-center justify-between shadow-md">
+        <div className="px-4 py-4 bg-white flex flex-row  items-center justify-between shadow-md">
           <div className="bg-white  shadow-lg rounded-full  p-4 text-center">
-            <MenuAlt1Icon className="h-5" />
+            <MenuAlt1Icon className="h-7 text-gray-700 cursor-pointer" />
           </div>
-          <div>
+          <div className=" flex items-center">
+            <div className="flex flex-col items-center">
+              <img src="/img/upload.png" />
+              <p>0 Planted</p>
+            </div>
             <span>D/L</span>
             <span>UPLOAD</span>
             <span>NOTIF</span>
