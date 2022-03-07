14f414dd7465dd77b5a5fbc2d00a86cef9b096ce
graphica99 <mendsalbert@gmail.com>
2022-03-07 19:12:42 +0000

    genopay ui

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import React from "react";
 import Layout from "./components/Layout/Layout";
 import trans from "./svgs/trans.svg";
+import wallet from "./svgs/wallet.svg";
 import {
   AreaChart,
   Area,
@@ -121,36 +122,36 @@ const App = () => {
             Your frequently used actions for easy access
           </p>
           <div className="grid grid-cols-2 gap-3">
-            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-4">
-              <img src="/img/addwallet.png" className="w-7" />
+            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-6">
+              <img src={wallet} className="w-10" />
 
               <p className="text-gray-500  ">Add new Wallet</p>
             </div>
-            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-4">
-              <img src="/img/addcard.png" className="w-7" />
+            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-6">
+              <img src={wallet} className="w-10" />
               <p className="text-gray-500  ">Add new Card</p>
             </div>
 
-            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-4">
-              <img src="/img/balanceexchange.png" className="w-7" />
+            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-6">
+              <img src={wallet} className="w-10" />
               <p className="text-gray-500  ">Balance Exchange</p>
             </div>
-            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-4">
-              <img src="/img/transfer.png" className="w-7" />
+            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-6">
+              <img src={wallet} className="w-10" />
               <p className="text-gray-500  ">transfer to Account</p>
             </div>
 
-            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-4">
-              <img src="/img/generate.png" className="w-7" />
+            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-6">
+              <img src={wallet} className="w-10" />
               <p className="text-gray-500  ">Generate Voucher</p>
             </div>
-            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-4">
-              <img src="/img/momo.png" className="w-7" />
+            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-6">
+              <img src={wallet} className="w-10" />
               <p className="text-gray-500  ">Mobile Money</p>
             </div>
 
-            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-4">
-              <img src="/img/payment_link.png" className="w-7" />
+            <div className="bg-white text-center border-2 items-center justify-center space-y-2 rounded-md flex flex-col p-6">
+              <img src={wallet} className="w-10" />
               <p className="text-gray-500  ">Payment Link</p>
             </div>
           </div>
