532bc97dd978cbe4f615fbab549638b5f653dd0d
graphica99 <mendsalbert@gmail.com>
2022-03-08 09:25:10 +0000

    genopay ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
+import ClockLoader from "react-spinners/ClockLoader";
 import { Routes, Route, Link } from "react-router-dom";
 import ScreenOne from "./pages/screenOne";
 import ScreenTwo from "./pages/screenTwo";
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<ScreenOne />} />
-      <Route path="wallet" element={<ScreenTwo />} />
-    </Routes>
-  );
-};
 
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-}
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  if (isLoading) {
+    return <p>Loding</p>;
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<ScreenOne />} />
+        <Route path="wallet" element={<ScreenTwo />} />
+      </Routes>
+    );
+  }
+};
 
 export default App;
